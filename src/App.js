51140d7b778a018b27d768c71bac625eb0e51a6c
Saurav Biswas <saru.sreyo@gmail.com>
2019-02-18 20:23:50 +0530

    Integrated chunking

--- src/App.js
+++ src/App.js
@@ -7,24 +7,31 @@ import MuiPickersUtilsProvider from 'material-ui-pickers/MuiPickersUtilsProvider
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import MomentUtils from '@date-io/moment';
 import {withRouter} from 'react-router-dom';
-import Research from './Research/Research';
-import StrategyDetail from './Research/StartegyDetail/StartegyDetail';
-import StrategyBacktests from './Research/StrategyBacktests/StrategyBacktests';
-import BacktestDetail from './Research/BacktestDetail/BacktestDetail';
-import Community from './Community/Community';
-import ThreadView from './Community/ThreadView/ThreadView';
-import NewPost from './Community/NewPost/NewPost';
-import Help from './HelpFrame';
-import Home from './HomeFrame';
-import Tutorial from './TutorialFrame';
-import ForbiddenAccess from './ErrorPages/ForbiddenAccess';
-import NoIternetAccess from './ErrorPages/NoIternetAccess';
-import BadRequest from './ErrorPages/BadRequest';
-import PageNotFound from './ErrorPages/PageNotFound';
-import FlowChartAlgo from './Research/FlowChartAlgo';
+import ChunkingLoader from './components/Loaders/ChunkingLoader';
 import './App.css';
 
 const {gaTrackingId} = require('./localConfig');
+
+/**
+ * Chunking code using React.Suspense and async imports
+ */
+
+const Research = React.lazy(() => import('./Research/Research'));
+const StrategyDetail = React.lazy(() => import('./Research/StartegyDetail/StartegyDetail'));
+const StrategyBacktests = React.lazy(() => import('./Research/StrategyBacktests/StrategyBacktests'));
+const BacktestDetail = React.lazy(() => import('./Research/BacktestDetail/BacktestDetail'));
+const Community = React.lazy(() => import('./Community/Community'));
+const ThreadView = React.lazy(() => import('./Community/ThreadView/ThreadView'));
+const NewPost = React.lazy(() => import('./Community/NewPost/NewPost'));
+const Help = React.lazy(() => import('./HelpFrame'));
+const Home = React.lazy(() => import('./HomeFrame'));
+const Tutorial = React.lazy(() => import('./TutorialFrame'));
+const ForbiddenAccess = React.lazy(() => import('./ErrorPages/ForbiddenAccess'));
+const NoIternetAccess = React.lazy(() => import('./ErrorPages/NoIternetAccess'));
+const BadRequest = React.lazy(() => import('./ErrorPages/BadRequest'));
+const PageNotFound = React.lazy(() => import('./ErrorPages/PageNotFound'));
+const FlowChartAlgo = React.lazy(() => import('./Research/FlowChartAlgo'));
+
 
 const theme = createMuiTheme({
 	palette: {
@@ -62,24 +69,26 @@ class App extends Component {
                         <Media 
                             query="(min-width: 801px)"
                             render={() => (
-                                <Switch>
-                                    <Route exact={true} path='/research' component={Research} />
-                                    <Route exact={true} path='/research/strategy/:strategyId' component={StrategyDetail} />
-                                    <Route exact={true} path='/research/backtests/:strategyId' component={StrategyBacktests} />
-                                    <Route exact={true} path='/research/backtests/:strategyId/:backtestId' component={BacktestDetail} />
-                                    <Route exact={true} path='/community' component={Community}/>
-                                    <Route exact={true} path='/community/postDetail/:postId' component={ThreadView} />
-                                    <Route exact={true} path='/community/newPost' component={NewPost} />
-                                    <Route exact={true} path='/help' component={Help}/>
-                                    <Route exact={true} path='/tutorial' component={Tutorial}/>
-                                    <Route exact={true} path='/home' component={Home}/>
-                                    <Route exact={true} path='/' component={Home}/>
-                                    <Route exact={true} path='/forbiddenAccess' component={ForbiddenAccess} />
-                                    <Route exact={true} path='/errorPage' component={NoIternetAccess} />
-                                    <Route exact={true} path='/badRequest' component={BadRequest} />
-                                    <Route exact={true} path='/algo' component={FlowChartAlgo} />
-                                    <Route component={PageNotFound} />
-                                </Switch>
+                                <React.Suspense fallback={<ChunkingLoader />}>
+                                    <Switch>
+                                        <Route exact={true} path='/research' component={Research} />
+                                        <Route exact={true} path='/research/strategy/:strategyId' component={StrategyDetail} />
+                                        <Route exact={true} path='/research/backtests/:strategyId' component={StrategyBacktests} />
+                                        <Route exact={true} path='/research/backtests/:strategyId/:backtestId' component={BacktestDetail} />
+                                        <Route exact={true} path='/community' component={Community}/>
+                                        <Route exact={true} path='/community/postDetail/:postId' component={ThreadView} />
+                                        <Route exact={true} path='/community/newPost' component={NewPost} />
+                                        <Route exact={true} path='/help' component={Help}/>
+                                        <Route exact={true} path='/tutorial' component={Tutorial}/>
+                                        <Route exact={true} path='/home' component={Home}/>
+                                        <Route exact={true} path='/' component={Home}/>
+                                        <Route exact={true} path='/forbiddenAccess' component={ForbiddenAccess} />
+                                        <Route exact={true} path='/errorPage' component={NoIternetAccess} />
+                                        <Route exact={true} path='/badRequest' component={BadRequest} />
+                                        <Route exact={true} path='/algo' component={FlowChartAlgo} />
+                                        <Route component={PageNotFound} />
+                                    </Switch>
+                                </React.Suspense>
                             )}
                         />
                     </div>
---
+++ src/components/Loaders/ChunkingLoader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import _ from 'lodash';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+export default class ChunkingLoader extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (!_.isEqual(this.props, nextProps) || !_.isEqual(nextState, this.state)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    render() {
+        return (
+            <Grid 
+                    container
+                    className='loader-container' 
+                    style={{height: 'calc(100vh - 300px)', width: '100%', backgroundColor: '#fff'}} 
+                    alignItems="center" 
+                    justify="center"
+            >
+                <CircularProgress/>
+            </Grid>
+        );
+    }
+}
